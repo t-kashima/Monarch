e288a79abd258299f263ed5f3664ce4c0dad0df6
Kashima Takumi <kodokurider@gmail.com>
2015-07-05 03:01:23 +0900

    テストの準備中

---
+++ app/src/test/java/com/unuuu/monarchsample/MainActivityTest.java
@@ -0,0 +1,22 @@
+package com.unuuu.monarchsample;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, emulateSdk = 21)
+public class MainActivityTest {
+    @Before
+    public void setup() {
+    }
+
+    @Test
+    public void 基本のテスト() {
+        assertThat("基本のテスト").isEqualTo("基本のテスト");
+    }
+}
